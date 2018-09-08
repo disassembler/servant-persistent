ccfee05044434585ed5143b53f48950ea432662e
Samuel Leathers <samuel.leathers@iohk.io>
2018-09-08 14:03:09 -0400

    factor out dependencies

--- snack.nix
+++ snack.nix
@@ -1,53 +1,33 @@
 let
+  dependencies = [
+    "aeson"
+    "containers"
+    "ekg"
+    "formatting"
+    "http-client"
+    "katip"
+    "monad-logger"
+    "monad-metrics"
+    "mtl"
+    "persistent-postgresql"
+    "persistent-template"
+    "postgresql-simple"
+    "postgresql-simple-migration"
+    "resource-pool"
+    "servant-auth"
+    "servant-auth-server"
+    "servant-client"
+    "servant-js"
+    "servant-server"
+    "wai-middleware-metrics"
+  ];
   lib =
-    { src = ./src;
-      dependencies = [
-        "aeson"
-        "containers"
-        "ekg"
-        "formatting"
-        "http-client"
-        "katip"
-        "monad-logger"
-        "monad-metrics"
-        "mtl"
-        "persistent-postgresql"
-        "persistent-template"
-        "postgresql-simple"
-        "postgresql-simple-migration"
-        "resource-pool"
-        "servant-auth"
-        "servant-auth-server"
-        "servant-client"
-        "servant-js"
-        "servant-server"
-        "wai-middleware-metrics"
-      ];
+    { inherit dependencies;
+      src = ./src;
     };
 in
   { main = "Main";
+    inherit dependencies;
     src = ./app;
     packages = [ lib ];
-    dependencies = [
-      "aeson"
-      "containers"
-      "ekg"
-      "formatting"
-      "http-client"
-      "katip"
-      "monad-logger"
-      "monad-metrics"
-      "mtl"
-      "persistent-postgresql"
-      "persistent-template"
-      "postgresql-simple"
-      "postgresql-simple-migration"
-      "resource-pool"
-      "servant-auth"
-      "servant-auth-server"
-      "servant-client"
-      "servant-js"
-      "servant-server"
-      "wai-middleware-metrics"
-    ];
   }
